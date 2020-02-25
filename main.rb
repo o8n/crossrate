608d096e0acb5b7108ced18440d628f26a7b6d46
okamotchan <m.okamotchan@gmail.com>
2020-02-26 00:43:29 +0900

    add main.rb

--- main.rb
+++ main.rb
@@ -1,16 +1,30 @@
 # frozen_string_literal: true
 
-class CrossRate
-  attr_reader :bid, :offer
+class CrossRate1
+  attr_reader :a_bid, :b_offer
 
-  def initialize(bid, offer)
-    @bid = bid
-    @offer = offer
+  def initialize(a_bid, b_offer)
+    @a_bid = a_bid
+    @b_offer = b_offer
   end
 
-  def ratio
-    bid * offer
+  def bid
+    a_bid / b_offer
   end
 end
 
-puts CrossRate.new(220, 120).ratio
+class CrossRate2
+  attr_reader :a_offer, :b_bid
+
+  def initialize(a_offer, b_bid)
+    @a_offer = a_offer
+    @b_bid = b_bid
+  end
+
+  def offer
+    a_offer / b_bid
+  end
+end
+
+p CrossRate1.new(100.00, 0.9100).bid.round(2)
+p CrossRate2.new(100.05, 0.9095).offer.round(2)
